36dc34cbd3d5cec10e0129a777da030910de5a42
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-04-26 23:38:09 +0530

    ql -server

--- new-server/schemas/resolvers.js
+++ new-server/schemas/resolvers.js
@@ -1,13 +1,38 @@
-const { UserList } = require("./fakeData")
+const fs = require("fs");
+const { UserList, MovieList } = require("./fakeData")
 
 const resolvers = {
     Query: {
-        users() {
+        users: () => {
             return UserList;
+        },
+        user: (_, args) => {
+            const id = args.id
+
+            return UserList.find((user) => user.id === Number(id))
+        },
+        movies: () => {
+            return MovieList
+        },
+        movie: (_, args) => {
+            return MovieList.find(movie => movie.name === args.name)
         }
+    },
+    User: {
+        favouriteMovies: () => {
+            return MovieList.filter(movie => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010)
+        }
+    },
+    Mutation: {
+        createUser: (_, args) => {
+            const { username, name, nationality, age } = args.input
+            const res = UserList.push({ name, username, nationality, age, id: UserList.length + 1 })
+            const user = UserList.find(user => user.id === res)
+            console.log(user)
+            return user
+        },
+        
     }
 }
-
-
 
 module.exports = resolvers
--- new-server/schemas/typeDefs.js
+++ new-server/schemas/typeDefs.js
@@ -4,11 +4,44 @@ const typeDefs = gql`
     type User{
         id:ID!
         name:String!
+        username:String!
         age:Int!
-        nationality:String!
+        nationality:nationality!
+        friends:[User]
+        favouriteMovies:[Movie]
     }
+
+    type Movie{
+        id:ID!
+        name:String!
+        yearOfPublication:Int!
+        isInTheaters:Boolean!
+    }
+
+    input CreateUserInput{
+        name:String!
+        username:String!
+        age:Int!
+        nationality:nationality!
+    }
+
     type Query{
         users:[User!]!
+        user(id:ID!):User
+        movies:[Movie!]!
+        movie(name:String!):Movie!
+    }
+
+    type Mutation {
+        createUser(input:CreateUserInput):User
+    }
+
+    enum nationality{
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
     }
 `
 
